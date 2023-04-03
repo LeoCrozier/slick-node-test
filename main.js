99c03f72f5af45b3986c70feee3afda48035e2b8
leo <lfjcrozier@gmail.com>
2023-04-03 16:01:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        //centerMode: true,
+        vertical: false,
+        adaptiveHeight: false,
+
+
+    });
+});
